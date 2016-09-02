4655bddbac362e815610b4a730a5177b32165d86
Jonas Bunawan <jonas_bun@yahoo.com>
2016-09-02 23:45:22 +1000

    initial commit

---
+++ main.js
@@ -0,0 +1,70 @@
+/*var myName = "Chris";
+
+if(myName !== "John"){
+    console.log("How about that?");
+}else if(myName === "Jonas"){
+    console.log("Hello, Jonas!");
+}else{
+    console.log("Hello!");
+}
+console.log(myName);
+
+var myNumber = 20;
+
+if(myNumber > 20){
+    console.log("Your number is greater than 20!");
+}else if(myNumber < 20){
+    console.log("Your number is less than 20!");
+}else{
+    console.log("Your number is 20!");
+}
+
+var myNumber = 50;
+
+if(myNumber <= 20){
+    console.log("You got the first case!");
+}else if(myNumber >= 100){
+    console.log("You got the second case!");
+}else{
+    console.log("You got the final case.");
+}
+
+var myString = "Hello World";
+
+console.log(myString + ", how are you?");
+console.log(myString.length);
+console.log(myString[0] + myString[1] + myString[myString.length - 1]);
+
+var myNumber = 20;
+var myDecimal = 400.3665;
+
+var result = (5 + 7) / (2 - 1) * 5;
+console.log(result);
+
+var myBoolean = true;
+var invertedBoolean = !myBoolean;
+var result = myBoolean === invertedBoolean;
+
+if(result){
+    console.log("myBoolean === invertedBoolean");
+}
+
+var myNull = null;
+
+var myUndefined = undefined;
+var myUndefined;*/
+
+var a = 10;
+var b = 20;
+var c = 10;
+
+if(!(a === b)){
+    console.log("First Condition");
+}else{
+    console.log("Condition not met");
+}
+
+var name = prompt("Enter your name");
+alert("Hello, " + name);
+
+// isNaN() is a function to determine whether a value being passed is a number or not (isNaN stands for isNotaNumber)
